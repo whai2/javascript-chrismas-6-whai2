3c3f22ce5b64f6b3ec13113e51b41f7638180b69
whai2 <whai2125@gmail.com>
2023-11-10 13:51:43 +0900

    test(Order): 할인 전 총 주문 금액 출력, 기능 테스트

--- __tests__/OrderTest.js
+++ __tests__/OrderTest.js
@@ -12,7 +12,7 @@ describe("주문 클래스 테스트", () => {
     const logSpy = getLogSpy();
 
     const input = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1";
-    const order = new Order(input);
+    new Order(input);
 
     const logs = [
       "티본스테이크 1개",
@@ -24,5 +24,16 @@ describe("주문 클래스 테스트", () => {
     logs.forEach((log) => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
     });
+  });
+
+  test("할인 전 충주문 금액을 보여준다.", () => {
+    const logSpy = getLogSpy();
+
+    const input = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1";
+    new Order(input);
+
+    const log = "142,000원";
+
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
   });
 });
