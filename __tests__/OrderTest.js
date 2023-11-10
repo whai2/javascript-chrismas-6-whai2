b3ffa2d228a0f528fb18f0a30bf59fcd7b24ac16
whai2 <whai2125@gmail.com>
2023-11-10 15:16:48 +0900

    feat(order): 할인 전 총 주문 금액을 알려주는 기능 구현

--- __tests__/OrderTest.js
+++ __tests__/OrderTest.js
@@ -30,7 +30,7 @@ describe("주문 클래스 테스트", () => {
     const logSpy = getLogSpy();
 
     const input = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1";
-    new Order(input);
+    const order = new Order(input);
 
     const log = "142,000원";
 
--- src/Order.js
+++ src/Order.js
@@ -1,4 +1,5 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import { menu } from "./Menu.js";
 
 export class Order {
   #order //-를 포함하는 문자열
@@ -11,13 +12,58 @@ export class Order {
   #splitAsCommas(orders) {
     const split = orders.split(',');
     
+    let totalprice = 0;
     for (let i = 0; i < split.length; i++) {
-      this.#splitAsDash(split[i]);
+      const totalPrice = this.#splitAsDash(split[i]);
+      totalprice += totalPrice;
     }
+
+    const formatPrice = this.#formatCurrency(totalprice);
+    MissionUtils.Console.print(`${formatPrice}원`);
   }
 
   #splitAsDash(order) {
     const split = order.split('-');
-    MissionUtils.Console.print(`${split[0]} ${split[1]}개`);
+    const counts = this.#countValidater(split[1])
+
+    MissionUtils.Console.print(`${split[0]} ${counts}개`);
+
+    const totalPrice = this.#totalPrize(split[0], counts);
+    return totalPrice;
+  }
+
+  #totalPrize(name, counts) {
+    let totalprice = 0;
+    for (const category in menu) {
+      const eachPrice = this.#eachPrice(menu[category], name, counts)
+      totalprice += eachPrice;
+    }
+    return totalprice;
+  }
+
+  #eachPrice(category, name, counts) {
+    for (const eachMenu in category) {
+      if (eachMenu === name) {
+        const price = category[eachMenu];
+        console.log(price * counts)
+        return Number(price * counts);
+      }
+    }
+    return 0;
+  }
+
+  #countValidater(number) {
+    if (/^[+]?[1-9]\d*$/.test(number)) {
+      return Number(number);
+    } 
+    throw new Error(errorComments.bonus[0]);
+  }
+
+  #formatCurrency(number) {
+    return new Intl.NumberFormat("ko-KR", {
+      style: "currency",
+      currency: "KRW",
+    minimumFractionDigits: 0, 
+    }).format(number).replace(/₩/g, '');
   }
 }
