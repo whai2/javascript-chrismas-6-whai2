a2a4cbfd28745f3dd45059f1a239eae908827b4f
whai2 <whai2125@gmail.com>
2023-11-14 21:52:12 +0900

    chore(badge): if문, switch문으로 수정

--- src/domain/Badge.js
+++ src/domain/Badge.js
@@ -8,19 +8,21 @@ export class Badge {
 
   #badgePrint() {
     const totalEventPrice = model.discountprice + model.giftprice;
-    this.#eachBadgePrint(totalEventPrice);
+    this.#eachBadgeStorage(totalEventPrice);
   }
 
-  #eachBadgePrint(totalEventPrice) {
+  #eachBadgeStorage(totalEventPrice) {
     const badgeList = ["별", "트리", "산타"];
-    if (5000 <= totalEventPrice && totalEventPrice < 10000) {
-      model.badge = badgeList[0];
-    }
-    if (10000 <= totalEventPrice && totalEventPrice < 20000) {
-      model.badge = badgeList[1];
-    }
-    if (20000 <= totalEventPrice) {
-      model.badge = badgeList[2];
+    switch (true) {
+      case (5000 <= totalEventPrice && totalEventPrice < 10000):
+        model.badge = badgeList[0];
+        break;
+      case (10000 <= totalEventPrice && totalEventPrice < 20000):
+        model.badge = badgeList[1];
+        break;
+      case (20000 <= totalEventPrice):
+        model.badge = badgeList[2];
+        break;
     }
   }
 }
